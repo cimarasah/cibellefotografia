5ae6da32a87581f0de1e810ed492c92c6ea4223d
c.de.sa.oliveira <c.de.sa.oliveira@WIN-8EY5DHKQX8>
2018-01-03 18:58:39 -0300

    Segunda versão

--- cs.cibellefotografia/cs.cibellefotografia/App_Start/BundleConfig.cs
+++ cs.cibellefotografia/cs.cibellefotografia/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace cs.cibellefotografia
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/detalhes.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ cs.cibellefotografia/cs.cibellefotografia/obj/Release/Package/PackageTmp/Scripts/detalhes.js
@@ -0,0 +1,14 @@
+﻿
+var imgs = ["../../img/capa/capaCasal.jpg", "../../img/capa/capaCriancaBanco.jpg", "../../img/capa/capaPegadas.jpg",
+    "../../img/capa/capaCriancaIgreja.jpg", "../../img/capa/capaPorDoSol.jpg"];
+
+setInterval(function () {
+    $("#hero").fadeOut(1000, function () {
+        $(this).css({ 'background-image': 'url(' + imgs.shift() + ')' }).fadeIn();
+    });
+    if (imgs.length == 0) {
+        imgs = ["../../img/capa/capaCasal.jpg", "../../img/capa/capaCriancaBanco.jpg", "../../img/capa/capaPegadas.jpg",
+            "../../img/capa/capaCriancaIgreja.jpg", "../../img/capa/capaPorDoSol.jpg"];
+
+    }
+}, 10000);
